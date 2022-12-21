3503d66feed3419bca57aa6c855a6eee46ea5800
Pavan Kalyan <pavankalyanm23@gmail.com>
2022-12-21 18:48:32 +0530

    project added

---
+++ components/Layout.jsx
@@ -0,0 +1,26 @@
+import Head from 'next/head';
+import React from 'react';
+import Footer from './Footer';
+import NavBar from './NavBar';
+
+const Layout = ({children}) => {
+  return (
+    <div className='layout'>
+      <Head>
+        <title>My Store</title>
+      </Head>
+      <header>
+        <NavBar /> 
+      </header>
+      <main className='main-container'>
+        {children}
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+
+    </div>
+  )
+}
+
+export default Layout
